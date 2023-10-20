e21aa319eeea3966f4318a5f52d91a0c1f4320e3
LZDEV <81957303+Lestradez@users.noreply.github.com>
2023-10-20 15:20:36 -0300

    docs: :memo: informe fast excel reader

--- vista/fast-excel-reader/index.php
+++ vista/fast-excel-reader/index.php
@@ -3,18 +3,127 @@ include_once __DIR__.'/../../includes/configuracion.php';
 include_once(STRUCTURE_PATH.'head.php');
 ?>
 <main class="p-5 text-center bg-light">
+  <h1 class="mb-3">Librería Fast Excel Reader</h1>
   
-  <h1>reCAPTCHA Library</h1>
-  <p>The reCAPTCHA library is a free service provided by Google that helps protect websites from spam and abuse. It uses advanced risk analysis techniques to tell humans and bots apart.</p>
-  <h2>How it works</h2>
-  <p>When a user visits a website that uses reCAPTCHA, they are presented with a challenge that is easy for humans to solve but difficult for bots. This challenge can be in the form of a checkbox, image recognition, or audio challenge.</p>
-  <p>Once the user completes the challenge, reCAPTCHA sends a token to the website's server, which can then be used to verify that the user is not a bot.</p>
-  <h2>Getting started</h2>
-  <p>To get started with reCAPTCHA, you will need to sign up for an API key from the <a href="https://www.google.com/recaptcha/admin/create">reCAPTCHA admin console</a>. Once you have your API key, you can add reCAPTCHA to your website by following the <a href="https://developers.google.com/recaptcha/docs/display">displaying reCAPTCHA documentation</a>.</p>
+  <div class="card shadow w-75 m-auto mb-3">
+    <div class="card-header">
+      <div class="row gx-3 gy-3 ">
+        <h2 > ¿Qué es Fast Excel Reader? </h2>  
+      </div>
+    </div>
+    <div class="card-body text-start">
+      <p>Esta librería puede leer hojas de cálculo compatibles con Excel en formato XLSX. Solo puede leer datos, pero lo hace de manera muy rápida y con muy poco uso de memoría.</p>
+      <p><strong>Características:</strong></p>
+<ul class="align-content-start">
+  <li class="align-self-start">Admite la autodetección de tipos de monedas / numéricos / tipos de fechas</li>
+  <li>Brinda un autoformato o uno personalizado para los tipos de fechas.</li>
+  <li>La librería puede definir y extraer imágenes de archivos XLSX</li>
+  <li>La librería puede leer diferentes estilos de celdas (formato de patrones, colores, bordes, fuentes, etc).</li> 
+</ul>
+        
+      </div>
+  </div>
+        
+  <div class="card shadow w-75 m-auto mb-3">
+    <div class="card-header">
+      <div class="row gx-3 gy-3 ">
+        <h2> Implementación </h2>  
+      </div>
+    </div>
+    <div class="card-body text-start">
+      <p><strong>Tiene dos formas de instalarse: </strong></p>
+      <ul>
+        <li>
+          <p><strong>Utilizando Composer.</strong></p>
+          <p>Composer es una herramienta para la gestión de dependencias en PHP. Le permite declarar las bibliotecas de las que depende su proyecto y las administrará (instalará/actualizará) automáticamente.
+            <br>Para descargarla pueden ir al siguiente link y descargar el instalador que se encargará del resto: <a href="https://getcomposer.org/download/" target="_blank">https://getcomposer.org/download/</a> 
+            <br>Una vez instalado, en la carpeta de su proyecto, ejecutar el siguiente comando: <code>composer require avadim/fast-excel-reader</code>
+        </p> 
+        </li>
+        <li>
+          <p><strong>
+            Descargando el paquete.
+          </strong></p>
+          <p>Para descargar el paquete, pueden ir al siguiente link: <a href="https://getcomposer.org/download/" target="_blank">https://getcomposer.org/download/</a> 
+            <br>Una vez descargado, descomprimirlo en la carpeta de su proyecto y agregar la siguiente línea:
+            <br><code>require_once __DIR__ . '/avadim/fast-excel-reader/src/FastExcelReader.php';</code>
+          </p>
+          <p><strong>Ejemplos de implementación:</strong></p>
+          <ul>
+            <li>
+              <p><strong>Cabecera:</strong></p>
+            <code >
+            <pre>
+&lt;?php
+  include_once __DIR__."/../../includes/configuracion.php";
+  include_once (STRUCTURE_PATH."head.php");
+  include_once  __DIR__.'/../../vendor/avadim/fast-excel-reader/src/autoload.php';
+  $file = __DIR__.'/../../uploads/planilla.xlsx';
+  $excel = \avadim\FastExcelReader\Excel::open($file);
+  $excel->setDateFormat('d-m-Y');
+  $result = $excel->readRows(true);
+  $sheet = $excel->sheet();
+  $firstRow = $sheet->readFirstRow();
+?&gt;
+            </pre>
+            </code>
+            </li>
+            <li>
+              <p><strong>Recorriendo el excel:</strong></p>
+              <code><pre>
+&lt;table class="table table-bordered table-striped table-hover"&gt;
+  &lt;thead class="border"&gt;
+    &lt;tr&gt;
+      &lt;?php
+          echo '&lt;th class="border-2 border-top-0 border-start-0 table-light" scope="col"&gt;&lt;/th&gt;';
+          foreach ($firstRow as $key => $value) {
+            echo '&lt;th class="border-2 border-top-0 table-light" scope="col"&gt;'.$key.'&lt;/th&gt;';
+          }
+      ?&gt;        
+    &lt;/tr&gt;
+    &lt;tr&gt;
+      &lt;?php
+        echo '&lt;td class="border-2 border-start-0 table-light fw-bold" scope="col"&gt;'.($sheet->firstRow()-1).'&lt;/td&gt;';
+        foreach ($firstRow as $key => $value) {
+          echo '&lt;th scope="col"&gt;'.$value.'&lt;/th&gt;';
+        }
+      ?&gt;
+    &lt;/tr&gt;
+  &lt;/thead&gt;
+  &lt;tbody class="border"&gt;
+    &lt;?php
+      foreach ($result as $cellKey => $cellValue) {    
+        echo '&lt;tr&gt;';
+        echo '&lt;td class="border-2 border-start-0 table-light fw-bold"&gt;'.$cellKey.'&lt;/td&gt;';
+        foreach ($firstRow as $rowKey => $rowValue) {
+          echo isset($cellValue[$rowValue])? '&lt;td&gt;'.$cellValue[$rowValue].'&lt;/td&gt;':'&lt;td&gt;&lt;/td&gt;';
+        }
+        echo '&lt;/tr&gt;';
+      }
+    ?&gt;
+  &lt;/tbody&gt;
+&lt;/table&gt;
+              </pre></code>
+            </li>
+          </ul>
+        </li>
+      </ul>
 
-
+    </div>
+  </div>
+  <div class="card shadow w-75 m-auto mb-3">
+    <div class="card-header">
+      <div class="row gx-3 gy-3 ">
+        <h2>   </h2>  
+      </div>
+    </div>
+    <div class="card-body">
+      <p></p>
+    </div>
+  </div>
 </main>
-  
+
 <?php
-  include STRUCTURE_PATH.'footer.php';
-?>
+    include STRUCTURE_PATH.'footer.php';
+    ?>
+  
